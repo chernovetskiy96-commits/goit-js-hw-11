4819c72008c61c54ffc407f2810a8ae689a15d8d
Yevhenii Chernov <yevheniichernov@MacBook-Air-de-Yevhenii.local>
2026-03-20 19:02:32 +0200

    edit

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,20 +1,17 @@
+import axios from "axios";
+
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '55106557-17a58ac065e1340b333cc7179';
 
 export function getImagesByQuery(query) {
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: query,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
-
-  return fetch(`${BASE_URL}?${params}`).then(response => {
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    return response.json();
-  });
-}
+  return axios.get(BASE_URL, {
+    params: {
+      key: API_KEY,
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+    },
+  })
+  .then(response => response.data);
+};
